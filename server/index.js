04ae9734f2d5aacb919a5c4c417f92044552c163
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-27 13:22:54 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/main.js"));
+});
+
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/styles.css"));
+});
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
